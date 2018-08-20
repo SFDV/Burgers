2a6277ab74f55aabf4b72769656ebd61bcb3dced
SuperFamousDanV <djudev777@live.com>
2018-08-19 23:59:16 -0400

    Moved to new repo to satisfy heroku

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Zuckerburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Bazinga Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Soy', FALSE);
